2b3395b00b8480c74e3aa775d6cf0d4d6dcec5a4
Sean Furlong <seanworkcomputer@Seans-MacBook-Pro.local>
2022-06-23 10:56:52 -0400

    first commit

---
+++ src/game/board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './board.css';
+
+function Board() {
+  return (
+    <div className="App">
+      <h1 className="App-header">Welcome To Tic Tac Toe</h1>
+    </div>
+  );
+}
+
+export default Board;
--- src/test/board.test.tsx
+++ src/test/board.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Board from '../game/board';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Board />);
+  const linkElement = screen.getByText(/Welcome To Tic Tac Toe/i);
   expect(linkElement).toBeInTheDocument();
 });
