f4c7bf0ccd89f45d86ee94d1e97ed52144cf095e
Cristian Baeza Ibarra <cristianbaeza092@gmail.com>
2022-07-06 13:18:20 -0600

    PR edits: test edits so they pass

--- src/components/board/board.test.tsx
+++ src/components/board/board.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import {Board} from '../board/board'
+import * as tlreact from "@testing-library/react";
+
 
 describe('board', () => {
   it('returns a blank board with nine cells', () => {
@@ -16,26 +18,23 @@ describe('board', () => {
   });
 
   it('after first click on board produces an X', () => {
-    render(<Board  />)
-    const cells = screen.getAllByRole('button')
-    const cellsToClick = cells.find(c => c.id === "cell_0")
-    expect(cellsToClick?.textContent).toBe("_")
-
+    tlreact.render(<Board />);
+    const cellsToClick = tlreact.screen.getByTestId('cell_0')
+    
+    expect(cellsToClick.textContent).toBe("_")
     fireEvent.click(cellsToClick)
-
+    
     expect(cellsToClick?.textContent).toBe("X")
   });
 
   it('after second click the player token swithes to O', () => {
-    render(<Board  />)
-    const cells = screen.getAllByRole('button')
-    const cellsToClick = cells.find(c => c.id === "cell_0")
-    expect(cellsToClick?.textContent).toBe("_")
-
+    tlreact.render(<Board />);
+    const cellsToClick = tlreact.screen.getByTestId('cell_0')
+    
+    expect(cellsToClick.textContent).toBe("_")
     fireEvent.click(cellsToClick)
     fireEvent.click(cellsToClick)
 
     expect(cellsToClick?.textContent).toBe("O")
   })
-
 });
